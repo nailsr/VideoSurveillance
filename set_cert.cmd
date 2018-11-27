b50c2113bf61d11a9f4ad5253e8e970727ad9639
Nail S <nailsr@gmail.com>
2018-11-27 13:57:17 +0400

    Add files via upload

---
+++ set_cert.cmd
@@ -0,0 +1,2 @@
+netsh http add sslcert ipport=0.0.0.0:7443 certhash=a909502dd82ae41433e6f83886b00d4277a32a7b appid={c7850af8-e5fb-4061-967d-7628b34c74ab}
+netsh http add sslcert ipport=[::]:7443 certhash=a909502dd82ae41433e6f83886b00d4277a32a7b appid={c7850af8-e5fb-4061-967d-7628b34c74ab}
