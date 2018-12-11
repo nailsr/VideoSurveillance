e134cd6bffe047b3efafde139d4338ac0a9e1c7c
Nail S <nailsr@gmail.com>
2018-12-11 15:19:45 +0400

    Rename reg_prefix.cmd to Command Tools/reg_prefix.cmd

---
+++ Command Tools/reg_prefix.cmd
@@ -0,0 +1,1 @@
+netsh http add urlacl url="https://vs.contoso.com:4443/" user="NT AUTHORITY\LOCAL SERVICE"
--- reg_prefix.cmd
+++
@@ -1,1 +0,0 @@
-netsh http add urlacl url="https://vs.contoso.com:4443/" user="NT AUTHORITY\LOCAL SERVICE"
